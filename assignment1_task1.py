b9d6fe9b1912eb17b174829e985eeccd3737cd38
dawncodes25 <devikadinesh25@gmail.com>
2025-05-02 07:43:25 +0530

    Add files via upload

---
+++ assignment1_task1.py
@@ -0,0 +1,9 @@
+a=input("Enter a number: ")
+b=input("Enter another number: ")
+a=float(a)
+b=float(b)
+
+print("Addition: ", (a +b))
+print("Subtraction: ", (a-b))
+print("Multiplication: ", (a*b))
+print("Divison: ", (a/b))
---
+++ assignment1_task2.py
@@ -0,0 +1,3 @@
+name1=input("Please enter your first name: ")
+name2=input("Please enter your last name: ")
+print("Hello " + name1 + " " + name2 + "!")
